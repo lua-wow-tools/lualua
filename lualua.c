421fb81c55cf9db558e9df1652bc31576b171cd6
Ferronn <ferronn@ferronn.dev>
2022-10-03 19:10:22 +0000

    pushlfunction

--- lualua.c
+++ lualua.c
@@ -3,7 +3,9 @@
 
 typedef struct {
   lua_State *state;
+  lua_State *host;
   int stackmax;
+  int hostuserdataref;
 } lualua_State;
 
 static const char lualua_state_metatable[] = "lualua state";
@@ -12,8 +14,12 @@ static int lualua_newstate(lua_State *L) {
   lualua_State *p = lua_newuserdata(L, sizeof(*p));
   luaL_getmetatable(L, lualua_state_metatable);
   lua_setmetatable(L, -2);
+  lua_pushvalue(L, -1);
+  int ref = luaL_ref(L, LUA_REGISTRYINDEX);
   p->state = luaL_newstate();
+  p->host = L;
   p->stackmax = LUA_MINSTACK;
+  p->hostuserdataref = ref;
   return 1;
 }
 
@@ -44,6 +50,7 @@ static void lualua_checkspace(lua_State *L, lualua_State *S, int space) {
 static int lualua_state_gc(lua_State *L) {
   lualua_State *S = lualua_checkstate(L, 1);
   lua_close(S->state);
+  luaL_unref(L, LUA_REGISTRYINDEX, S->hostuserdataref);
   return 0;
 }
 
@@ -232,6 +239,30 @@ static int lualua_pushboolean(lua_State *L) {
   return 0;
 }
 
+static int lualua_invokefromregistry(lua_State *SS) {
+  lualua_State *S = lua_touserdata(SS, lua_upvalueindex(1));
+  int hostfunref = lua_tonumber(SS, lua_upvalueindex(2));
+  lua_State *L = S->host;
+  lua_rawgeti(L, LUA_REGISTRYINDEX, hostfunref);
+  lua_rawgeti(L, LUA_REGISTRYINDEX, S->hostuserdataref);
+  int value = lua_pcall(L, 1, 1, 0);
+  if (value != 0) {
+    lua_error(L);
+  }
+  return lua_tonumber(L, -1);
+}
+
+static int lualua_pushlfunction(lua_State *L) {
+  lualua_State *S = lualua_checkstate(L, 1);
+  luaL_argcheck(L, lua_isfunction(L, 2), 2, "function expected");
+  lualua_checkspace(L, S, 2);
+  int ref = luaL_ref(L, LUA_REGISTRYINDEX);
+  lua_pushlightuserdata(S->state, S);
+  lua_pushnumber(S->state, ref); /* TODO GC */
+  lua_pushcclosure(S->state, lualua_invokefromregistry, 2);
+  return 0;
+}
+
 static int lualua_pushnil(lua_State *L) {
   lualua_State *S = lualua_checkstate(L, 1);
   lualua_checkspace(L, S, 1);
@@ -340,6 +371,7 @@ static const struct luaL_Reg lualua_state_index[] = {
     {"pcall", lualua_pcall},
     {"pop", lualua_pop},
     {"pushboolean", lualua_pushboolean},
+    {"pushlfunction", lualua_pushlfunction},
     {"pushnil", lualua_pushnil},
     {"pushnumber", lualua_pushnumber},
     {"pushstring", lualua_pushstring},
