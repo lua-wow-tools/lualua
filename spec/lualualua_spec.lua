cb4d6773b6ae8889ea789b75f0f644bbc97f2da5
Ferronn <ferronn@ferronn.dev>
2022-10-13 19:15:23 +0000

    just do the printing from sandbox

--- spec/lualualua_spec.lua
+++ spec/lualualua_spec.lua
@@ -3,10 +3,11 @@ describe('lualualua', function()
     local s = require('lualua').newstate()
     s:openlibs()
     s:loadstring([=[
-      local errors = {}
       local path = {}
       local function onerror(s)
-        errors[table.concat(path, ' ')] = s
+        print()
+        print(table.concat(path, ' '))
+        print(s)
       end
       local function busted(name, fn)
         table.insert(path, name)
@@ -16,7 +17,6 @@ describe('lualualua', function()
       assert = require('luassert')
       describe = busted
       it = busted
-      return errors
     ]=])
     s:call(0, 1)
     s:loadstring([=[
@@ -33,16 +33,5 @@ describe('lualualua', function()
     local lualuaspec = require('pl.file').read('spec/lualua_spec.lua')
     s:loadstring(lualuaspec, '@spec/lualua_spec.lua')
     s:call(0, 0)
-    s:pushnil()
-    local count = 0
-    while s:next(-2) do
-      print()
-      print(s:tostring(-2))
-      print(s:tostring(-1))
-      s:pop(1)
-      count = count + 1
-    end
-    print()
-    print('lualualua errors: ' .. count)
   end)
 end)
